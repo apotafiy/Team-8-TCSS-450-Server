f23d18286a7ccfaf95fc5b00922ca523676e94f6
Artem Potafiy <apotafiy@uw.edu>
2021-11-01 16:51:01 -0700

    small non-functional changes like adding comment and changing spacing

--- routes/signin.js
+++ routes/signin.js
@@ -59,6 +59,7 @@ router.get(
   },
   (request, response, next) => {
     // obtain auth credentials from HTTP Header
+    // TODO: not sure what this does
     const base64Credentials = request.headers.authorization.split(' ')[1];
 
     const credentials = Buffer.from(base64Credentials, 'base64').toString(
