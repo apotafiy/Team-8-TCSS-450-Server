867e400332a7ad68f78f5d52ef5cff213a76530c
JeffSchultz99 <jeffschultz99@gmail.com>
2021-11-24 15:29:04 -0800

    Update chatinfo.js

--- routes/chatinfo.js
+++ routes/chatinfo.js
@@ -10,35 +10,21 @@ router.use(require("body-parser").json())
 
 router.get("/", (request, response) => {
     let query = 'SELECT ChatID, Name FROM Chats where ChatID in (SELECT ChatID FROM ChatMembers where MemberID=$1)'
-    let values = [request.decoded.memberid]
-
-    pool.query(query, values)
-    .then(result => {
-        if (result.rowCount == 0) {
-            response.status(404).send({
-                message: "No messages"
-            })
-        } else {
-            let chatList = [];
-            result.rows.forEach(entry =>
-                chatList.push(
-                    {
-                        "chat": entry.chatid,
-                        "name": entry.name
-                    }
-                )
-            )
-            response.send({
-                success: true,
-                chats: chatList
-            })
-        }
-    }).catch(error => {
+    let values = [request.decoded.memberid];
+    console.log(request.decoded.memberid);
+    pool
+      .query(query, values)
+      .then((result) => {
+        response.send({
+          rows: result.rows,
+        });
+      })
+      .catch((error) => {
         response.status(400).send({
-            message: "SQL Error",
-            error:error
-        })
-    })
-});
+          message: 'SQL Error when searching for contacts,',
+          error: error,
+        });
+      });
+  });
 
 module.exports = router 
